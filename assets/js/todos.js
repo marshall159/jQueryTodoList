023c6181e9f69f3aec75dfc34e640395323a5431
Aneel Marshall <aneel.marshall@outlook.com>
2017-06-01 08:48:16 +0100

    Change jQuery file from slim to full for fadeOut feature

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,4 +1,12 @@
+//Check off specific todos by clicking
 $('li').on('click', function() {
 	$(this).toggleClass('completed');
 });
 
+//Click on X to delete todo
+$('span').on('click', function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
