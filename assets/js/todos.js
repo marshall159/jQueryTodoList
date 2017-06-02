3114a54a2401a02696ea49a012b3b7285ac74687
Aneel Marshall <aneel.marshall@outlook.com>
2017-06-02 11:47:37 +0100

    Add styling: delete icons, font, background, margins

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -16,7 +16,7 @@ $("input[type='text']").on('keypress', function(event) {
 	if (event.which === 13) {
 		var todoText = $(this).val();
 		$(this).val('');
-		$('ul').append('<li><span>X</span> ' + todoText + '</li>');
+		$('ul').append('<li><span><i class="fa fa-trash-o" aria-hidden="true"></i></span> ' + todoText + '</li>');
 	}
 
 });
